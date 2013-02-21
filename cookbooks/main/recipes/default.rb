75cd99dbe1c1e56db8ef5f091af76f2c8178afee
John Maya <jcmaya@hotmail.com>
2013-02-20 23:40:34 -0500

    initial

---
+++ cookbooks/main/recipes/default.rb
@@ -0,0 +1,14 @@
+package "git-core"
+package "vim"
+
+
+user node[:user][:name] do
+	password node[:user][:password]
+	#gid "admin"
+	home "/home/#{node[:user][:name]}"
+	supports manage_home:true
+	shell "/bin/bash"
+end
+group "admin" do
+	members ['deployer']
+end
