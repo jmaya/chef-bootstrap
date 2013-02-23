9b36941577bc366d07b544242d12c18fd19a55b4
John Maya <jcmaya@hotmail.com>
2013-02-23 10:01:37 -0500

    updated to ruby-1.9.3-p392.tar.gz

--- chef-bootstrap.sh
+++ chef-bootstrap.sh
@@ -2,9 +2,9 @@
 apt-get -y update
 apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 cd /tmp
-wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p385.tar.gz
-tar -xvzf ruby-1.9.3-p385.tar.gz
-cd ruby-1.9.3-p385/
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz
+tar -xvzf ruby-1.9.3-p392.tar.gz
+cd ruby-1.9.3-p392/
 ./configure --prefix=/usr/local
 make
 make install
